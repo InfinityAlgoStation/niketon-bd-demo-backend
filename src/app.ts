2777e966fe06f7fcb1d9dbb43c0490a79e4054ef
sabbir1054 <mdsabbir1054@gmail.com>
2023-12-18 02:48:16 +0600

    dep change again 3

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.use('/api/v1/', routes);
 
 // testing
 app.get('/', async (req: Request, res: Response) => {
-  res.send('Server is running ✅✅✅');
+  res.send('Server is running auto change is detect 📌 ');
 });
 
 // global error handler middleware
